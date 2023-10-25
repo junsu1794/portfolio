dcaeaa50209c8ade9692c3652afe841d49e84f12
junsu1794 <sphw7700@nate.com>
2023-10-25 11:34:10 +0900

    scroll

---
+++ scroll.js
@@ -0,0 +1,47 @@
+const win = $(window);
+const sections = $('section');
+let speed = Math.floor(win.height() * 0.5);
+let topArr = [];
+let winSCT;
+console.log(sections);
+console.log(sections.offset().top);
+
+sections.each(function (i, o) {
+	const sectionTop = $(o).offset().top;
+	topArr.push(sectionTop);
+});
+win.on('scroll', () => {
+	winSCT = win.scrollTop();
+
+	if (winSCT > topArr[0] - speed) {
+		sections.eq(0).addClass('is-animated');
+		pipScroll();
+	}
+	if (winSCT > topArr[1] - speed) {
+		sections.eq(1).addClass('is-animated');
+		pipScroll();
+	}
+});
+
+function pipScroll() {
+	const devices = $('.mockup.pc, .mockup.mobile');
+	devices.each(function (i, deviceEl) {
+		let device = $(this);
+		let screen = device.find('.mask>img');
+		const mask = device.find('.mask');
+		const hightDifference = screen.innerHeight() - mask.innerHeight();
+		console.log('hightDifference', hightDifference);
+		device.on({
+			mouseenter: function () {
+				screen.stop().animate({ top: -hightDifference }, 1500);
+			},
+			mouseleave: function () {
+				screen.stop().animate({ top: 0 }, 1500);
+			},
+		});
+	});
+}
+win.on('resize', function () {
+	pipScroll();
+});
+
