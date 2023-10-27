152ca1f31fa58491a8d707b3be34aec1662c8bbc
junsu1794 <sphw7700@nate.com>
2023-10-27 16:58:33 +0900

    수정

--- scroll.js
+++ scroll.js
@@ -45,3 +45,11 @@ win.on('resize', function () {
 	pipScroll();
 });
 
+//ani
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('lottie',), // Required
+	path: '/ani.json', // Required
+	renderer: 'svg', // Required
+	loop: true, // Optional
+	autoplay: true, // Optional
+})
